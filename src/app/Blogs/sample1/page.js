23108d242d610243cf3844dc1443522bdc25e8cf
Himanshu <himanshu212223@gmail.com>
2024-09-20 18:17:59 +0530

    adding sample code

--- src/app/Blogs/sample1/page.js
+++ src/app/Blogs/sample1/page.js
@@ -1,9 +1,21 @@
+import PrismLoader from "@/Components/prism-loader";
+
 const sample1 = () => {
-    return(
+    return (
         <>
             Welcome to sample1
+
+            <pre className="language-js line-numbers">
+                <code className="language-js">
+                    console.log("hello world");
+                </code>
+            </pre>
+            <pre className="language-ts line-numbers">
+                <code className="language-ts">console.log("hello world")</code>
+            </pre>
+            <PrismLoader />
         </>
     );
 }
 
-export default sample1 ;
+export default sample1;
