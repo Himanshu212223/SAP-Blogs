ac15559bb20e205886a76f8e1e557eca51f2084c
Himanshu <himanshu212223@gmail.com>
2024-12-31 23:48:49 +0530

    created separate folder for Fiori Blogs

--- src/Resources/contentList.js
+++ src/Resources/contentList.js
@@ -1,43 +1,43 @@
 export const fiori = [
     {
         title : "Logged In User Details",
-        link : '/Blogs/LoggedInUserDetails' 
+        link : '/Blogs/Fiori/LoggedInUserDetails' 
     },
     {
         title : "Connect Destination with Fiori UI5",
-        link : '/Blogs/ConnectDestinationWithFiori' 
+        link : '/Blogs/Fiori/ConnectDestinationWithFiori' 
     },
     {
         title : "Import JSON To Fiori",
-        link : '/Blogs/ImportJsonFile' 
+        link : '/Blogs/Fiori/ImportJsonFile' 
     },
     {
         title : "Import Excel To Fiori",
-        link : '/Blogs/ImportExcelToFiori' 
+        link : '/Blogs/Fiori/ImportExcelToFiori' 
     },
     {
         title : "Formatter on Table",
-        link : '/Blogs/FormatterOnTable' 
+        link : '/Blogs/Fiori/FormatterOnTable' 
     },
     {
         title : "Search and Sort on Table",
-        link : '/Blogs/SearchAndSort' 
+        link : '/Blogs/Fiori/SearchAndSort' 
     },
     {
         title : "Routing using Base Controller",
-        link : '/Blogs/RoutingUsingBaseController' 
+        link : '/Blogs/Fiori/RoutingUsingBaseController' 
     },
     {
         title : "Routing & Navigation",
-        link : '/Blogs/Routing' 
+        link : '/Blogs/Fiori/Routing' 
     },
     {
         title : "Configure JSON Model in UI5 Dynamically",
-        link : '/Blogs/ConnectToJsonModelDynamically' 
+        link : '/Blogs/Fiori/ConnectToJsonModelDynamically' 
     },
     {
         title : "Configure JSON Model with Fiori UI5 Application",
-        link : '/Blogs/ConnectToJsonModel' 
+        link : '/Blogs/Fiori/ConnectToJsonModel' 
     }
 ] ;
 
